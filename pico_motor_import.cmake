33b3c9254474629e19293899de5dc20c4241a399
Compdog Inc. <compdog.inc@gmail.com>
2024-09-28 14:53:35 -0400

    Fix cmake

--- pico_motor_import.cmake
+++
@@ -1,6 +0,0 @@
-add_subdirectory(pico-motor)
-link_directories(${CMAKE_CURRENT_LIST_DIR}/pico-motor)
-
-macro(pico_motor_init target)
-    target_include_directories(target PRIVATE ${CMAKE_CURRENT_LIST_DIR}/pico-motor/include)
-endmacro()
