f9e394ea7333ad167b78056a6d27698753ec8ce7
Compdog Inc. <compdog.inc@gmail.com>
2024-12-10 00:33:52 -0500

    Add customizable frequency and resolution of pwm

--- include/motor.h
+++ include/motor.h
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <hardware/pio.h>
 
+constexpr uint32_t MAX_MOTOR_PWM_RESOLUTION = (1u << 16) - 1;     // 16-bit pwm resolution is the highest supported resolution (up to around 600 Hz)
+constexpr uint32_t DEFAULT_MOTOR_PWM_RESOLUTION = (1u << 11) - 1; // 11-bit pwm resolution (up to around 20 kHz - best for motors)
+
 /// @brief A PWM DC motor controller using PIO
 class Motor
 {
@@ -11,16 +14,33 @@ public:
     /// @brief Create a new motor
     /// @param cw The clockwise GPIO pin
     /// @param ccw The counter-clockwise GPIO pin
+    /// @param frequency The frequency of the PWM in Hz - default is 20 kHz
+    /// @param resolution The resolution (max counter value) of the PWM - default is 11-bit, just enough for 20 kHz
+    /// @note The lower the resolution, the higher the maximum reachable frequency is, but with less speed steps
     /// @warning The two pins have to be next to each other!
-    Motor(uint cw, uint ccw);
+    Motor(uint cw, uint ccw, float frequency = 20000.0f /* 20 kHz */, uint32_t resolution = DEFAULT_MOTOR_PWM_RESOLUTION);
     ~Motor();
 
     /// @brief Set the motor percent speed
     /// @param speed The speed (-1 to 1)
     void set(float speed);
-
     /// @brief Returns the currently set percent speed (-1 to 1)
-    float getSetSpeed();
+    float getSetSpeed() const;
+
+    /// @brief Sets the frequency of the motor PWM in Hz
+    /// @param frequency The frequency in Hz
+    void setFrequency(float frequency);
+    /// @brief Returns the current frequency of the motor PWM in Hz
+    float getFrequency() const;
+
+    void setResolution(uint32_t resolution);
+    uint32_t getResolution() const;
+
+    /// @brief Returns the maximum reachable frequency in Hz with the current PWM resolution
+    float maxAllowedFrequency() const;
+
+    /// @brief Returns the maximum reachable frequency in Hz with the provided PWM resolution
+    static float maxAllowedFrequency(uint32_t resolution);
 
 private:
     PIO pio;
@@ -28,6 +48,8 @@ private:
     uint base;
     bool baseIsCcw;
     float currentSpeed;
+    float frequency;
+    uint32_t resolution;
 };
 
 #endif
--- src/motor.cpp
+++ src/motor.cpp
@@ -2,6 +2,7 @@
 #include <limits.h>
 #include <math.h>
 #include <hardware/pio.h>
+#include <hardware/clocks.h>
 #include "pio/driverpwm.h"
 #include "motor.h"
 #include "config.h"
@@ -17,9 +18,34 @@ static uint8_t smIndex = 0;
 static const uint8_t sm_values[] = {
     USED_STATE_MACHINES};
 
-constexpr uint32_t MOTOR_PWM_PERIOD = (1u << 16) - 1; // 16-bit pwm resolution
+inline uint32_t get_cycles_from_resolution(uint32_t resolution)
+{
+    return 3 * resolution + 10;
+}
 
-Motor::Motor(uint cw, uint ccw) : currentSpeed(0)
+inline float get_clkdiv_from_hz(float hz, uint32_t resolution)
+{
+    uint32_t clk = clock_get_hz(clk_sys);
+    return (float)clk / (get_cycles_from_resolution(resolution) * hz);
+}
+
+inline float get_hz_from_clkdiv(float clkdiv, uint32_t resolution)
+{
+    // inverse of itself
+    return get_clkdiv_from_hz(clkdiv, resolution);
+}
+
+float Motor::maxAllowedFrequency() const
+{
+    return get_hz_from_clkdiv(1, resolution); // clkdiv can't go lower than 1
+}
+
+float Motor::maxAllowedFrequency(uint32_t resolution)
+{
+    return get_hz_from_clkdiv(1, resolution); // clkdiv can't go lower than 1
+}
+
+Motor::Motor(uint cw, uint ccw, float frequency, uint32_t resolution) : currentSpeed(0), frequency(frequency), resolution(resolution)
 {
     // pio program uses a base+offset pin assignment
     if (cw < ccw)
@@ -37,6 +63,11 @@ Motor::Motor(uint cw, uint ccw) : currentSpeed(0)
 
     assert(smIndex < sizeof(sm_values));
 
+    assert(resolution >= 0);
+    assert(resolution <= MAX_MOTOR_PWM_RESOLUTION);
+    assert(frequency > 0);
+    assert(frequency <= maxAllowedFrequency());
+
     sm = sm_values[smIndex++];
     pio = sm < 4 ? pio0 : pio1; // pio0 is sm 0 - 3, pio1 is 4 - 6
     sm %= 4;                    // sm index relative to pio
@@ -53,8 +84,8 @@ Motor::Motor(uint cw, uint ccw) : currentSpeed(0)
 
     // init pio program with base pin
     driverpwm_program_init(pio, sm, pio == pio0 ? pio0Offset : pio1Offset, base);
-    pio_driverpwm_set_config(pio, sm, MOTOR_PWM_PERIOD);
-    pio_sm_set_clkdiv(pio, sm, 20.f);
+    pio_driverpwm_set_config(pio, sm, resolution);
+    pio_sm_set_clkdiv(pio, sm, get_clkdiv_from_hz(frequency, resolution));
     set(0);
 }
 
@@ -71,7 +102,7 @@ void Motor::set(float speed)
 
     currentSpeed = speed;
     speed *= (speed > 0 ? 1 : -1) * speed;                        // Squared PWM response curve, with sign
-    uint16_t level = (uint16_t)floorf(fabsf(speed) * UINT16_MAX); // pio program uses 16-bit speed values
+    uint16_t level = (uint16_t)floorf(fabsf(speed) * resolution); // pio program uses raw counter values, scale by speed
     if (speed >= 0)
     {
         pio_driverpwm_set_level(pio, sm, level, baseIsCcw);
@@ -82,7 +113,34 @@ void Motor::set(float speed)
     }
 }
 
-float Motor::getSetSpeed()
+float Motor::getSetSpeed() const
 {
     return currentSpeed;
+}
+
+void Motor::setFrequency(float frequency)
+{
+    assert(frequency > 0);
+    assert(frequency <= maxAllowedFrequency());
+    this->frequency = frequency;
+    pio_sm_set_clkdiv(pio, sm, get_clkdiv_from_hz(frequency, resolution));
+}
+
+float Motor::getFrequency() const
+{
+    return frequency;
+}
+
+void Motor::setResolution(uint32_t resolution)
+{
+    assert(resolution >= 0);
+    assert(resolution <= MAX_MOTOR_PWM_RESOLUTION);
+    this->resolution = resolution;
+    pio_driverpwm_set_config(pio, sm, resolution);
+    pio_sm_set_clkdiv(pio, sm, get_clkdiv_from_hz(frequency, resolution));
+}
+
+uint32_t Motor::getResolution() const
+{
+    return resolution;
 }
